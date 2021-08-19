306f77efaece0b7c664d0042098604e3f7048013
Anthony Cros <cros.anthony@gmail.com>
2021-08-19 14:22:15 -0400

    split TargetDomain.scala in two

---
+++ src/main/scala/gallia/target/HasTargetQuerySeq.scala
@@ -0,0 +1,38 @@
+package gallia.target
+
+import aptus.Anything_
+
+import gallia._
+import gallia.target.utils.TypedTargetQueryUtils
+import gallia.vldt.MetaValidation
+
+// ===========================================================================
+trait HasTargetQuerySeq[$Target] {
+  def tqs: Seq[TargetQuery[$Target]]
+
+  final def  tqkpaths                   (implicit ev: $Target <:< KPath): Seq[TqKPath]  = tqs                    .map(_.asInstanceOf[TqKPath])
+  final def ttqkpaths(value: HasTypeSeq)(implicit ev: $Target <:< KPath): Seq[TtqKPath] = tqkpaths.zip(value.hts).map(x => TypedTargetQueryUtils.ttqkpath1(x._1, x._2))
+
+  // ---------------------------------------------------------------------------
+  def targets(c: Cls): Seq[$Target] = tqs.map(_.resolve(c))
+  
+  // TODO: should be private
+  /*private[target] */def __kpathz(c: Cls): KPathz = tqs.flatMap(_.__kpaths(c)).thn(KPathz.apply)
+  /*private[target] */def __qpathz(c: Cls): RPathz = tqs.flatMap(_.__qpaths(c)).thn(RPathz.apply)
+  
+  // ---------------------------------------------------------------------------
+  def pathz(c: Cls)(implicit ev: $Target <:< KPath): KPathz = KPathz(targets(c).asInstanceOf[Seq[KPath]])
+
+  // ===========================================================================
+  // vldt
+
+  private[target] def _vldtAsOrigin(c: Cls): Errs =
+      tqs.foldLeft(Seq[Err]())(_ ++ _.vldtAsOrigin(c)) ++ 
+      MetaValidation.distinctRPathz(__qpathz(c))
+
+  private[target] def _vldtAsNewDestination(c: Cls): Errs =
+    tqs.foldLeft(Seq[Err]())(_ ++ _.vldtAsNewDestination(c)) ++ 
+    MetaValidation.distinctRPathz(__qpathz(c))
+}
+
+// ===========================================================================

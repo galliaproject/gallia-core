d8ee9b0afa7c7d30baf024129bcaa7bda83e19d4
Anthony Cros <cros.anthony@gmail.com>
2021-10-29 16:40:43 -0400

    fixed DAG field transcience (for serialization)

--- src/main/scala/gallia/dag/DAG.scala
+++ src/main/scala/gallia/dag/DAG.scala
@@ -11,7 +11,7 @@ class DAG[$NodeType](
         val nodes     : Seq[$NodeType], // will always be relatively "small" (eg not millions of nodes)
         val edges     : Seq[Edge],      // will be pretty sparse so no need for density matrix
         val idResolver: $NodeType => NodeId) // less painful than subtyping...
-      extends Serializable {
+      extends Serializable /* some Atoms use sub-plans, meaning we need to be able to serialize the DAG */ {
     private type Self = DAG[$NodeType]
 
     // TODO: (t210116120349 - easier with graph lib?)
@@ -19,12 +19,12 @@ class DAG[$NodeType](
     // - check no isolates - TODO: will create issues for chain-traversal... (see 201123165502)
     // - check nodes are DOT friendly (see t210114113316)
 
-    @transient private[dag] val _lookup: Map[NodeId, $NodeType] = nodes.map(_.associateLeft(idResolver)).force.map
+    @transient private[dag] lazy val _lookup: Map[NodeId, $NodeType] = nodes.map(_.associateLeft(idResolver)).force.map
 
     def lookup(nodeId: NodeId): $NodeType = _lookup(nodeId) // used in ASG creation
 
-                 val nodeIds  : Seq[NodeId] = nodes.map(idResolver) // used in Handler's "join2"
-    private[dag] val nodeIdSet: Set[NodeId] = nodeIds.toSet
+    @transient              lazy val nodeIds  : Seq[NodeId] = nodes.map(idResolver) // used in Handler's "join2"
+    @transient private[dag] lazy val nodeIdSet: Set[NodeId] = nodeIds.toSet
 
     // ---------------------------------------------------------------------------
     require(
@@ -66,21 +66,21 @@ class DAG[$NodeType](
       private def defaultDotStyle(node: $NodeType) = (idResolver(node), "White")
 
     // ===========================================================================
-    private[dag] val _efferents: Map[NodeId, Nes[NodeId]] = edges.groupBy(_._1).mapValues(_.map(_._2)).toMap
-    private[dag] val _afferents: Map[NodeId, Nes[NodeId]] = edges.groupBy(_._2).mapValues(_.map(_._1)).toMap
+    @transient private[dag] lazy val _efferents: Map[NodeId, Nes[NodeId]] = edges.groupBy(_._1).mapValues(_.map(_._2)).toMap
+    @transient private[dag] lazy val _afferents: Map[NodeId, Nes[NodeId]] = edges.groupBy(_._2).mapValues(_.map(_._1)).toMap
 
     // ---------------------------------------------------------------------------
-    private      def kahnTraverseIds  : Seq[NodeId]    = if (nodeIds.size <= 1) nodeIds else GraphUtils.kahn(edges.sorted).right.get.toList// ChainUtils.chainTraversal(this)(this.rootIds).map(idResolver)      
-            lazy val kahnTraverseNodes: Seq[$NodeType] = kahnTraverseIds.map(_lookup)
+    private         def kahnTraverseIds  : Seq[NodeId]    = if (nodeIds.size <= 1) nodeIds else GraphUtils.kahn(edges.sorted).right.get.toList// ChainUtils.chainTraversal(this)(this.rootIds).map(idResolver)      
+    @transient lazy val kahnTraverseNodes: Seq[$NodeType] = kahnTraverseIds.map(_lookup)
 
     def chainTraverseNodes: Seq[$NodeType] = ChainUtils.chainTraversal(this)(this.rootIds)
             
     // ---------------------------------------------------------------------------
-    lazy val roots : Seq[$NodeType] = nodeIds.diff(_afferents.keys.toSeq).map(_lookup)
-    lazy val leaves: Seq[$NodeType] = nodeIds.diff(_efferents.keys.toSeq).map(_lookup)
+    @transient lazy val roots : Seq[$NodeType] = nodeIds.diff(_afferents.keys.toSeq).map(_lookup)
+    @transient lazy val leaves: Seq[$NodeType] = nodeIds.diff(_efferents.keys.toSeq).map(_lookup)
 
-    lazy val rootIds : Seq[NodeId] = roots .map(idResolver)
-    lazy val leaveIds: Seq[NodeId] = leaves.map(idResolver)
+    @transient lazy val rootIds : Seq[NodeId] = roots .map(idResolver)
+    @transient lazy val leaveIds: Seq[NodeId] = leaves.map(idResolver)
 
     def forceLeaf: LeafId = leaveIds.force.one
 
@@ -104,9 +104,10 @@ class DAG[$NodeType](
     def trimLeaf(leaf: $NodeType) = removeNodes(Set(leaf))
 
     // ---------------------------------------------------------------------------
-         def isEmpty  : Boolean = nodes.isEmpty
-         def isTrivial: Boolean = nodes.size == 1
-    lazy val isChain  : Boolean = 
+    def isEmpty  : Boolean = nodes.isEmpty
+    def isTrivial: Boolean = nodes.size == 1
+
+    @transient lazy val isChain: Boolean = 
       _afferents.forall(_._2.size == 1) && 
       _efferents.forall(_._2.size == 1)
 

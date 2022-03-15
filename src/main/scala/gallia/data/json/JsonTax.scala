422f466cfb646b7ad25f01cdb6cd6942672c6d5b
Anthony Cros <cros.anthony@gmail.com>
2022-03-15 14:21:14 -0400

    Generalized Json tax (numbers, dates, ...)

--- src/main/scala/gallia/data/json/JsonTax.scala
+++ src/main/scala/gallia/data/json/JsonTax.scala
@@ -2,31 +2,68 @@ package gallia
 package data.json
 
 import meta._
+import aptus._
 
 // ===========================================================================
-object JsonNumberTax extends atoms.utils.ModifyObj { // because of 201119115427; this is a high price to pay to support integers...
+object JsonTax {
+
+  def payAllUp(c: Cls) =    
+            (JsonNumberTax.payUp(c) _)
+    //TODO: t220315141543 - optim: skip if schema doesn't contain bignums/times            
+    .andThen(BigIntJsonTax.payUp(c) _)
+    .andThen(BigDecJsonTax.payUp(c) _)
+    .andThen(LocalDataJsonTax.payUp(c) _)  
+}
+
+// ===========================================================================
+trait JsonTax extends atoms.utils.ModifyObj {
+  type T
+  
+  // ---------------------------------------------------------------------------
+  def qualifies(field: Fld): Boolean
+  def transformValue(qualityingField: Fld)(value: T): Any
 
   // ---------------------------------------------------------------------------
-  def payUp(c: Cls)(o: Obj): Obj = super.modify(c)(o)
+  final def payUp(c: Cls)(o: Obj): Obj = super.modify(c)(o)
 
   // ===========================================================================
   @NumberAbstraction
-  override protected def qualifyingFields(c: Cls): Seq[Fld] =
+  final override protected def qualifyingFields(c: Cls): Seq[Fld] =
     c
       .fields
       .filter { field =>
         !field.isStringDoubleBoolean && // most will stop here (optimization)
-        ( field.isIntegerLikeType || 
+        ( qualifies(field) || 
           field.nestedClassOpt.exists { qualifyingFields(_).nonEmpty }) }
 
   // ===========================================================================
-  override protected def transformation(qualifyingField: Fld): Any => Any =
+  final override protected def transformation(qualifyingField: Fld): Any => Any =
     (qualifyingField.isMultiple, qualifyingField.nestedClassOpt) match {
-      case (false, None)     => _.asInstanceOf[    Double ].pipe(qualifyingField.forceIntegerLikeType.toIntegerLike)
-      case (true , None)     => _.asInstanceOf[Seq[Double]].map (qualifyingField.forceIntegerLikeType.toIntegerLike)
+      case (false, None)     => _.asInstanceOf[    T ]     .pipe(transformValue(qualifyingField))
+      case (true , None)     => _.asInstanceOf[Seq[T]]     .map (transformValue(qualifyingField))
       case (false, Some(c2)) => _.asInstanceOf[    Obj    ].pipe(payUp(c2))
       case (true , Some(c2)) => _.asInstanceOf[Seq[Obj   ]].map (payUp(c2)) }
 
 }
+
+// ===========================================================================
+object JsonNumberTax extends JsonTax { type T = Double // because of 201119115427; this is a high price to pay to support integers...  
+  def qualifies(field: Fld): Boolean = field.isIntegerLikeType
+  def transformValue(qualitifyingField: Fld)(value: T): Any  = qualitifyingField.forceIntegerLikeType.toIntegerLike(value) }
+
+// ---------------------------------------------------------------------------
+object BigIntJsonTax extends JsonTax { type T = String
+    def qualifies(field: Fld): Boolean = field.isBigInteger
+    def transformValue(ignored: Fld)(value: T): Any  = BigInt.apply(value) }
+  
+  // ---------------------------------------------------------------------------
+  object BigDecJsonTax extends JsonTax { type T = String
+    def qualifies(field: Fld): Boolean = field.isBigDecimal
+    def transformValue(ignored: Fld)(value: T): Any  = BigDecimal.apply(value) }
+
+// ---------------------------------------------------------------------------
+object LocalDataJsonTax extends JsonTax { type T = String
+    def qualifies(field: Fld): Boolean = field.isDate 
+    def transformValue(ignored: Fld)(value: T): Any  = value.toLocalDateFromIso }
 
 // ===========================================================================
---
+++ src/main/scala/gallia/run/GenericRunners.scala
@@ -0,0 +1,35 @@
+package gallia
+package run
+
+// ===========================================================================
+private[gallia] class GenericRunnerO(writing: AObj => Unit) {
+
+    def run(head: HeadO): Unit =
+        head.runGeneric(GenericRunnerOutputU)
+
+      // ---------------------------------------------------------------------------
+      object GenericRunnerOutputU extends ActionUOc with IdentityVM1 {
+          def atomuo(c: Cls) = _GenericRunnerOutputU(c) }
+
+        // ---------------------------------------------------------------------------
+        case class _GenericRunnerOutputU(c: Cls) extends AtomUO {
+          def naive(o: Obj) = writing(AObj(c, o)) }
+  }
+
+  // ===========================================================================
+  private[gallia] class GenericRunnerZ(writing: AObjs => Unit) {
+
+    def run(head: HeadS): Unit =
+        head.runGeneric(GenericRunnerOutputZ)
+
+      // ---------------------------------------------------------------------------
+      object GenericRunnerOutputZ extends ActionZOc with IdentityVM1 {
+          def atomzo(c: Cls) = _GenericRunnerOutputZ(c) }
+
+        // ---------------------------------------------------------------------------
+        case class _GenericRunnerOutputZ(c: Cls) extends AtomZO {
+          def naive(z: Objs) = writing(AObjs(c, z)) }
+
+  }
+
+// ===========================================================================

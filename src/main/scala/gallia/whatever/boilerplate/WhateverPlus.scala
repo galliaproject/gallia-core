1ba0bb31057e63fddc70b56415bb59a48710ab9f
Anthony Cros <cros.anthony@gmail.com>
2022-03-18 18:16:01 -0400

    Renamings

--- src/main/scala/gallia/whatever/boilerplate/WhateverPlus.scala
+++ src/main/scala/gallia/whatever/boilerplate/WhateverPlus.scala
@@ -11,92 +11,92 @@ object WhateverPlus {
       
       // ---------------------------------------------------------------------------
       case x: Int => second match {
-        case y: Int        => (x + y): Int
-        case y: Double     => (x + y): Double
+        case y: Int    => (x + y): Int
+        case y: Double => (x + y): Double
         
-        case y: Byte       => (x + y): Int
-        case y: Short      => (x + y): Int
-        case y: Long       => (x + y): Long
+        case y: Byte   => (x + y): Int
+        case y: Short  => (x + y): Int
+        case y: Long   => (x + y): Long
 
-        case y: Float      => (x + y): Float
+        case y: Float  => (x + y): Float
 
-        case y: BigInt     => (x + y): BigInt
-        case y: BigDecimal => (x + y): BigDecimal }
+        case y: BigInt => (x + y): BigInt
+        case y: BigDec => (x + y): BigDec }
       
       // ---------------------------------------------------------------------------
       case x: Double => second match {
-        case y: Int        => (x + y): Double
-        case y: Double     => (x + y): Double
+        case y: Int    => (x + y): Double
+        case y: Double => (x + y): Double
         
-        case y: BigInt     => (BigDecimal(x) + BigDecimal(y)): BigDecimal
-        case y: BigDecimal => (x + y)                        : BigDecimal
+        case y: BigInt => (BigDec    (x) + BigDec    (y)): BigDec    
+        case y: BigDec => (x + y)                        : BigDec    
         
-        case y: Number     => (x + y.doubleValue()): Double }
+        case y: Number => (x + y.doubleValue()): Double }
 
       // ---------------------------------------------------------------------------
       case x: Byte => second match {
-        case y: Int        => (x + y): Int
-        case y: Double     => (x + y): Double
+        case y: Int    => (x + y): Int
+        case y: Double => (x + y): Double
         
-        case y: Byte       => (x + y): Int // see 210809113923
-        case y: Short      => (x + y): Int // see 210809113923
-        case y: Long       => (x + y): Long
+        case y: Byte   => (x + y): Int // see 210809113923
+        case y: Short  => (x + y): Int // see 210809113923
+        case y: Long   => (x + y): Long
 
-        case y: Float      => (x + y): Float
+        case y: Float  => (x + y): Float
 
-        case y: BigInt     => (x + y): BigInt
-        case y: BigDecimal => (x + y): BigDecimal }
+        case y: BigInt => (x + y): BigInt
+        case y: BigDec => (x + y): BigDec }
       
       // ---------------------------------------------------------------------------
       case x: Short => second match {
-        case y: Int        => (x + y): Int
-        case y: Double     => (x + y): Double
+        case y: Int    => (x + y): Int
+        case y: Double => (x + y): Double
         
-        case y: Byte       => (x + y): Int // see 210809113923
-        case y: Short      => (x + y): Int // see 210809113923
-        case y: Long       => (x + y): Long
+        case y: Byte   => (x + y): Int // see 210809113923
+        case y: Short  => (x + y): Int // see 210809113923
+        case y: Long   => (x + y): Long
 
-        case y: Float      => (x + y): Float
+        case y: Float  => (x + y): Float
 
-        case y: BigInt     => (x + y): BigInt
-        case y: BigDecimal => (x + y): BigDecimal }
+        case y: BigInt => (x + y): BigInt
+        case y: BigDec => (x + y): BigDec }
       
       // ---------------------------------------------------------------------------
       case x: Long => second match {
-        case y: Double     => (x + y)             : Double
-        case y: Float      => (x + y)             : Float
+        case y: Double => (x + y)             : Double
+        case y: Float  => (x + y)             : Float
 
-        case y: BigInt     => (BigInt(x) + y)     : BigInt
-        case y: BigDecimal => (x + y)             : BigDecimal
+        case y: BigInt => (BigInt(x) + y)     : BigInt
+        case y: BigDec => (x + y)             : BigDec    
         
-        case y: Number     => (x + y.floatValue()): Float }
+        case y: Number => (x + y.floatValue()): Float }
       
       // ---------------------------------------------------------------------------
       case x: Float => second match {
-        case y: Double     => (x + y): Double
+        case y: Double => (x + y): Double
 
-        case y: BigInt     => (BigDecimal(x) + BigDecimal(y)): BigDecimal
-        case y: BigDecimal => (x + y)                        : BigDecimal
+        case y: BigInt => (BigDec    (x) + BigDec    (y)): BigDec    
+        case y: BigDec => (x + y)                        : BigDec    
         
-        case y: Number     => (x + y.floatValue()): Float }
+        case y: Number => (x + y.floatValue()): Float }
 
       // ---------------------------------------------------------------------------
       case x: BigInt => second match {          
-        case y: Int        => (x + y): BigInt          
-        case y: Byte       => (x + y): BigInt
-        case y: Short      => (x + y): BigInt
-        case y: Long       => (x + y): BigInt
-        case y: BigInt     => (x + y): BigInt
+        case y: Int    => (x + y): BigInt          
+        case y: Byte   => (x + y): BigInt
+        case y: Short  => (x + y): BigInt
+        case y: Long   => (x + y): BigInt
+        case y: BigInt => (x + y): BigInt
 
-        case y: Double     => (BigDecimal(x) + y): BigDecimal
-        case y: Float      => (BigDecimal(x) + y): BigDecimal
-        case y: BigDecimal => (BigDecimal(x) + y): BigDecimal }
+        case y: Double => (BigDec    (x) + y): BigDec    
+        case y: Float  => (BigDec    (x) + y): BigDec    
+        case y: BigDec => (BigDec    (x) + y): BigDec }
       
       // ---------------------------------------------------------------------------
-      case x: BigDecimal => second match {
-        case y: BigDecimal => (x + y)            : BigDecimal
-        case y: BigInt     => (x + BigDecimal(y)): BigDecimal
-        case y: Number     => (x + y.doubleValue): BigDecimal }
+      case x: BigDec     => second match {
+        case y: BigDec => (x + y)            : BigDec    
+        case y: BigInt => (x + BigDec    (y)): BigDec    
+        case y: Number => (x + y.doubleValue): BigDec }
     }
   }
 }

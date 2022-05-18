540c395eace5d25345e1dba146a3d610a008572a
Anthony Cros <cros.anthony@gmail.com>
2022-05-18 15:52:13 -0400

    Merged 'ModifyObj' into 'Modifytabledata'

--- src/main/scala/gallia/data/ModifyTableData.scala
+++ src/main/scala/gallia/data/ModifyTableData.scala
@@ -1,19 +1,27 @@
 package gallia
 package data
 
-import meta.Fld
+import meta._
 
 // ===========================================================================
-class ModifyTableData(conf: io.CellConf) extends atoms.utils.ModifyObj { // 201231113658
+class ModifyTableData(conf: io.CellConf) { // 201231113658
 
-  // ---------------------------------------------------------------------------
-  def modify(x: AObjs): Objs = x.z.map(super.modify(x.c))
+  def modify(value: AObjs): Objs =
+      value
+        .z
+        .map { o =>
+          qualifyingFields(value.c)
+            .foldLeft(o) { (curr, qualifyingField) =>
+              val key = qualifyingField.key
+
+              if (!curr.contains(key)) curr
+              else curr.transformPath(key, transformation(qualifyingField)) } }
 
     // ===========================================================================
-    override protected def qualifyingFields(c: Cls): Seq[Fld] = c.fields.filterNot(_.isOneString)
+    private def qualifyingFields(c: Cls): Seq[Fld] = c.fields.filterNot(_.isOneString)
 
       // ===========================================================================
-      override protected def transformation(qualifyingField: Fld): Any => Any =
+      private def transformation(qualifyingField: Fld): Any => Any =
         any => conf.transformValue(qualifyingField.subInfo1.isMultiple)(any.asInstanceOf[String]).fold(
           _.map /* option's */(conf.transformBasicValue(qualifyingField.forceBasicType)),
           _.map /* seq   's */(conf.transformBasicValue(qualifyingField.forceBasicType)) )

295c62048ac05ebc8248458f615760f32a096883
Anthony Cros <cros.anthony@gmail.com>
2022-10-07 11:18:47 -0400

    Added convenience builder for DataRegenerationClosure

--- src/main/scala/gallia/data/DataPackage.scala
+++ src/main/scala/gallia/data/DataPackage.scala
@@ -14,6 +14,13 @@ package object data {
   // ---------------------------------------------------------------------------
   /** mostly for Iterator based processing */
   trait DataRegenerationClosure[T] { def regenerate: () => aptus.CloseabledIterator[T] }
+
+    // ---------------------------------------------------------------------------
+    object DataRegenerationClosure {
+      def fromUncloseable[T](data: Iterator[T]) = new DataRegenerationClosure[T] {
+        def regenerate: () => aptus.CloseabledIterator[T] =
+          () => aptus.CloseabledIterator.fromUncloseable(data) } }
+
 }
 
 // ===========================================================================

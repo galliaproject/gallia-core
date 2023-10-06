3f2baa864ed607dae958f5c41305f5a630b1d25c
Anthony Cros <cros.anthony@gmail.com>
2023-10-06 09:17:13 -0400

    scala 3 migration - moved CompanionReflection to scala-{2,3} folders

---
+++ src/main/scala-3/gallia/reflect/CompanionReflection.scala
@@ -0,0 +1,9 @@
+package gallia
+package reflect
+
+// ===========================================================================
+private object CompanionReflection { 
+  def apply[T : WTT](methodName: String)(args: Object*): T = ??? }
+
+// ===========================================================================
+

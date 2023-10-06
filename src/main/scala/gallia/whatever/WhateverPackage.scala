06312ff9c6836b2500dd57654b8548a8723a3416
Anthony Cros <cros.anthony@gmail.com>
2023-10-06 12:02:26 -0400

    scala 3 migration - workaround for _.capitalize (final)

--- src/main/scala/gallia/whatever/WhateverPackage.scala
+++ src/main/scala/gallia/whatever/WhateverPackage.scala
@@ -2,6 +2,10 @@ package gallia
 
 // ===========================================================================
 package object whatever {
-  private[whatever] val StringOrdering = implicitly[Ordering[String]] }
+  private[whatever] val StringOrdering = implicitly[Ordering[String]]
+
+  // ---------------------------------------------------------------------------
+  /** else causes issues with scala 3 if used directly in Whatever.capitalize */
+  private[whatever] def capitalize(value: String): String = value.capitalize }
 
 // ===========================================================================

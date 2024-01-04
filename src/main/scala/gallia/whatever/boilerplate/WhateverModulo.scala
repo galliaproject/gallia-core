0d01b1ab4cfe72cdaf81c3922d8422e195d71899
Anthony Cros <cros.anthony@gmail.com>
2024-01-04 15:34:40 -0500

    swappped the two (filenames were mismatched with object names)

--- src/main/scala/gallia/whatever/boilerplate/WhateverModulo.scala
+++ src/main/scala/gallia/whatever/boilerplate/WhateverModulo.scala
@@ -3,101 +3,101 @@ package whatever
 package boilerplate
 
 // ===========================================================================
-object WhateverDividedBy {
+object WhateverModulo {
 
   /* keep in sync with other boilerplate's Whatever{Plus,Times,...} */
-  @NumberAbstraction 
-  private[gallia] def apply(first: Any, second: Any): Any = {     
-    first match { 
-      
+  @NumberAbstraction
+  private[gallia] def apply(first: Any, second: Any): Any = {
+    first match {
+
       // ---------------------------------------------------------------------------
       case x: Int => second match {
-        case y: Int    => (x / y): Int
-        case y: Double => (x / y): Double
-        
-        case y: Byte   => (x / y): Int
-        case y: Short  => (x / y): Int
-        case y: Long   => (x / y): Long
+        case y: Int    => (x % y): Int
+        case y: Double => (x % y): Double
 
-        case y: Float  => (x / y): Float
+        case y: Byte   => (x % y): Int
+        case y: Short  => (x % y): Int
+        case y: Long   => (x % y): Long
 
-        case y: BigInt => (x / y): BigInt
-        case y: BigDec => (x / y): BigDec }
-      
+        case y: Float  => (x % y): Float
+
+        case y: BigInt => (x % y): BigInt
+        case y: BigDec => (x % y): BigDec }
+
       // ---------------------------------------------------------------------------
       case x: Double => second match {
-        case y: Int    => (x / y): Double
-        case y: Double => (x / y): Double
-        
-        case y: BigInt => (BigDec    (x) / BigDec    (y)): BigDec    
-        case y: BigDec => (x / y)                        : BigDec    
-        
-        case y: Number => (x / y.doubleValue()): Double }
+        case y: Int    => (x % y): Double
+        case y: Double => (x % y): Double
+
+        case y: BigInt => (BigDec    (x) % BigDec    (y)): BigDec
+        case y: BigDec => (x % y)                        : BigDec
+
+        case y: Number => (x % y.doubleValue()): Double }
 
       // ---------------------------------------------------------------------------
       case x: Byte => second match {
-        case y: Int    => (x / y): Int
-        case y: Double => (x / y): Double
-        
-        case y: Byte   => (x / y): Int // see 210809113923
-        case y: Short  => (x / y): Int // see 210809113923
-        case y: Long   => (x / y): Long
+        case y: Int    => (x % y): Int
+        case y: Double => (x % y): Double
 
-        case y: Float  => (x / y): Float
+        case y: Byte   => (x % y): Int // see 210809113923
+        case y: Short  => (x % y): Int // see 210809113923
+        case y: Long   => (x % y): Long
 
-        case y: BigInt => BigInt(x) / y
-        case y: BigDec => BigDec(x) / y }
+        case y: Float  => (x % y): Float
+
+        case y: BigInt => BigInt(x) % y
+        case y: BigDec => BigDec(x) % y }
 
       // ---------------------------------------------------------------------------
       case x: Short => second match {
-        case y: Int    => (x / y): Int
-        case y: Double => (x / y): Double
-        
-        case y: Byte   => (x / y): Int // see 210809113923
-        case y: Short  => (x / y): Int // see 210809113923
-        case y: Long   => (x / y): Long
+        case y: Int    => (x % y): Int
+        case y: Double => (x % y): Double
 
-        case y: Float  => (x / y): Float
+        case y: Byte   => (x % y): Int // see 210809113923
+        case y: Short  => (x % y): Int // see 210809113923
+        case y: Long   => (x % y): Long
 
-        case y: BigInt => BigInt(x) / y
-        case y: BigDec => BigDec(x) / y }
-      
+        case y: Float  => (x % y): Float
+
+        case y: BigInt => BigInt(x) % y
+        case y: BigDec => BigDec(x) % y }
+
       // ---------------------------------------------------------------------------
       case x: Long => second match {
-        case y: Double => (x / y)             : Double
-        case y: Float  => (x / y)             : Float
+        case y: Double => (x % y)             : Double
+        case y: Float  => (x % y)             : Float
 
-        case y: BigInt => (BigInt(x) / y)     : BigInt
-        case y: BigDec => (x / y)             : BigDec    
-        
-        case y: Number => (x / y.floatValue()): Float }
-      
+        case y: BigInt => (BigInt(x) % y)     : BigInt
+        case y: BigDec => (x % y)             : BigDec
+
+        case y: Number => (x % y.floatValue()): Float }
+
       // ---------------------------------------------------------------------------
       case x: Float => second match {
-        case y: Double => (x / y): Double
+        case y: Double => (x % y): Double
 
-        case y: BigInt => BigDec(x) / BigDec(y)
-        case y: BigDec => BigDec(x) /        y
+        case y: BigInt => (BigDec(x) % BigDec(y)): BigDec
+        case y: BigDec => (BigDec(x) %        y ): BigDec
 
-        case y: Number => (x / y.floatValue()): Float }
+        case y: Number => (x % y.floatValue()): Float }
 
       // ---------------------------------------------------------------------------
-      case x: BigInt => second match {          
-        case y: Int    => (x / y): BigInt          
-        case y: Byte   =>  x / BigInt(y)
-        case y: Short  =>  x / BigInt(y)
-        case y: Long   => (x / y): BigInt
-        case y: BigInt => (x / y): BigInt
+      case x: BigInt => second match {
+        case y: Int    => (x %        y ): BigInt
+        case y: Byte   => (x % BigInt(y)): BigInt
+        case y: Short  => (x % BigInt(y)): BigInt
+        case y: Long   => (x %        y ): BigInt
+        case y: BigInt => (x %        y ): BigInt
 
-        case y: Double => (BigDec    (x) /        y ): BigDec
-        case y: Float  =>  BigDec    (x) / BigDec(y)
-        case y: BigDec => (BigDec    (x) /        y ): BigDec }
+        case y: Double => (BigDec    (x) %        y ): BigDec
+        case y: Float  => (BigDec    (x) % BigDec(y)): BigDec
+        case y: BigDec => (BigDec    (x) %        y ): BigDec }
 
       // ---------------------------------------------------------------------------
       case x: BigDec     => second match {
-        case y: BigDec => (x / y)            : BigDec    
-        case y: BigInt => (x / BigDec    (y)): BigDec    
-        case y: Number => (x / y.doubleValue): BigDec }
+        case y: BigDec => (x % y)            : BigDec
+        case y: BigInt => (x % BigDec    (y)): BigDec
+        case y: Number => (x % y.doubleValue): BigDec }
     }
   }
 }

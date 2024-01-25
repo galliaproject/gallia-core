4e1c61ed794303e91664abf2202523236aef4da3
Anthony Cros <cros.anthony@gmail.com>
2024-01-25 14:09:17 -0500

    minor improvements (2)

--- core/src/main/scala/gallia/actions/in/ActionsInMongoDb.scala
+++ core/src/main/scala/gallia/actions/in/ActionsInMongoDb.scala
@@ -8,11 +8,12 @@ case class MongodbInputZ(
       schemaProvider: OtherSchemaProvider,
       queryingOpt   : Option[ReadQuerying] /* None if URI-driven (eg "mydb.mycoll") */)
     extends ActionIZ01
-    with    HasSchemaProviderZ { //TODO: t201223092203 - look into https://docs.mongodb.com/realm/mongodb/document-schemas
+    with    HasSchemaProviderZ { // TODO: t201223092203 - look into https://docs.mongodb.com/realm/mongodb/document-schemas
   def vldt : Errs = Nil //TODO: check valid URI, ...
   def _meta: Cls  = __meta
 
   // ---------------------------------------------------------------------------
-  def hasCommonObjs = AtomsIX._MongodbInputZ(inputString, schemaProvider, queryingOpt, resultCls) } // TODO: t201214105653 - address resultCls hack
+  def hasCommonObjs = new AtomsIX._MongodbInputZ(inputString, queryingOpt, resultCls  )
+}
 
 // ===========================================================================

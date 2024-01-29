fe9c0a470c691c50bf57d6d6cbd830cf1df5a9a0
Anthony Cros <cros.anthony@gmail.com>
2024-01-29 12:34:43 -0500

    refactorings for Convert operations

--- core/src/main/scala/gallia/actions/common/ActionsCommonConverts.scala
+++ core/src/main/scala/gallia/actions/common/ActionsCommonConverts.scala
@@ -11,47 +11,58 @@ object ActionsCommonConverts {
   import utils.ActionsUtils._
 
   // ---------------------------------------------------------------------------
-  case class ConvertToString(target: TqRPathz) extends ActionUU1N {
-    def vldt   (c: Cls): Errs    = target.vldtAsOrigin(c) // TODO: validate can reasonably formatted to a string
-    def _meta  (c: Cls): Cls     = target.resolve(c).foldLeft(c) { _.transformSoleSubInfo(_)(_.copy(valueType = BasicType._String)) }
-    def atomuus(c: Cls): AtomUUs = target.resolve(c).pipe(_atoms(c)(_ConvertToString)) }
+  sealed trait ConverBase {
+    val target: TqRPathz
+
+    // ---------------------------------------------------------------------------
+    final def vldtAsOrigin(c: Cls): Errs   = target.vldtAsOrigin(c)
+    final def rpathz      (c: Cls): RPathz = target.resolve(c)
+
+    final def foldRpathz (c: Cls)(f: (Cls, RPath) => Cls): Cls = target.resolve(c).foldLeft(c)(f)
+    final def soleSubInfo(c: Cls)(f: SubInfo => SubInfo) : Cls = target.resolve(c).foldLeft(c) { _.transformSoleSubInfo(_)(f) } }
+
+  // ===========================================================================
+  case class ConvertToString(target: TqRPathz) extends ActionUU1N with ConverBase {
+    def vldt   (c: Cls): Errs    = vldtAsOrigin(c) // TODO: validate can reasonably formatted to a string
+    def _meta  (c: Cls): Cls     = soleSubInfo (c)(_.copy(valueType = BasicType._String))
+    def atomuus(c: Cls): AtomUUs = rpathz        (c).pipe(_atoms(c)(_ConvertToString)) }
 
   // ---------------------------------------------------------------------------
-  case class ConvertToEnum(target: TqRPathz, values: Seq[EnumValue]) extends ActionUU1N {
-    def vldt   (c: Cls): Errs    = target.vldtAsOrigin(c) // TODO: validate can reasonably formatted to a string
+  case class ConvertToEnum(target: TqRPathz, values: Seq[EnumValue]) extends ActionUU1N with ConverBase {
+    def vldt   (c: Cls): Errs    = vldtAsOrigin(c) // TODO: validate can reasonably formatted to a string
       .orIfEmpty { _vldt.checkAreValidEnumValues(values).toSeq }
-    def _meta  (c: Cls): Cls     = target.resolve(c).foldLeft(c) { _.transformSoleSubInfo(_)(_.copy(valueType = BasicType._Enm(values))) }
-    def atomuus(c: Cls): AtomUUs = target.resolve(c).pipe(_atoms(c)(_ConvertToEnum)) } // TODO: optim: only if not String field already
+    def _meta  (c: Cls): Cls     = soleSubInfo (c)(_.copy(valueType = BasicType._Enm(values)))
+    def atomuus(c: Cls): AtomUUs = rpathz(c).pipe(_atoms(c)(_ConvertToEnum)) } // TODO: optim: only if not String field already
 
   // ---------------------------------------------------------------------------
-  case class ConvertToInt(target: TqRPathz) extends ActionUU1Nb {
-    def vldt                     (c: Cls): Errs    = target.vldtAsOrigin(c)
-    def _meta                    (c: Cls): Cls     = target.resolve(c).foldLeft(c) { _.transformSoleSubInfo(_)(_.toInt) }
-    def atomuus(origin: CallSite)(c: Cls): AtomUUs = target.resolve(c).pipe(_atoms(c)(_ConvertToInt(origin))) } // TODO: t220929161636 - generalize callsite mechanism
+  case class ConvertToInt(target: TqRPathz) extends ActionUU1Nb with ConverBase {
+    def vldt                     (c: Cls): Errs    = vldtAsOrigin(c)
+    def _meta                    (c: Cls): Cls     = soleSubInfo (c)(_.toInt)
+    def atomuus(origin: CallSite)(c: Cls): AtomUUs = rpathz(c).pipe(_atoms(c)(_ConvertToInt(origin))) } // TODO: t220929161636 - generalize callsite mechanism
 
   // ---------------------------------------------------------------------------
-  case class ConvertToDouble(target: TqRPathz) extends ActionUU1Nb {
-    def vldt                     (c: Cls): Errs    = target.vldtAsOrigin(c)
-    def _meta                    (c: Cls): Cls     = target.resolve(c).foldLeft(c) { _.transformSoleSubInfo(_)(_.toDouble) }
-    def atomuus(origin: CallSite)(c: Cls): AtomUUs = target.resolve(c).pipe(_atoms(c)(_ConvertToDouble(origin))) }
+  case class ConvertToDouble(target: TqRPathz) extends ActionUU1Nb with ConverBase {
+    def vldt                     (c: Cls): Errs    = vldtAsOrigin(c)
+    def _meta                    (c: Cls): Cls     = soleSubInfo (c)(_.toDouble)
+    def atomuus(origin: CallSite)(c: Cls): AtomUUs = rpathz(c).pipe(_atoms(c)(_ConvertToDouble(origin))) }
 
   // ---------------------------------------------------------------------------
-  case class ConvertToFlag[T: WTT](target: TqRPathz, trueValue: T, strict: Boolean) extends ActionUU1Nb {
-    def vldt                     (c: Cls): Errs    = target.vldtAsOrigin(c)
-    def _meta                    (c: Cls): Cls     = target.resolve(c).foldLeft(c) { _.updateInfo(_, Info.optBoolean) } // TODO: t210108114447 - support own "flag" type?
-    def atomuus(origin: CallSite)(c: Cls): AtomUUs = target.resolve(c).pipe(_atoms(c)(_ConvertToFlag(origin)(_, trueValue, strict))) }
+  case class ConvertToFlag[T: WTT](target: TqRPathz, trueValue: T, strict: Boolean) extends ActionUU1Nb with ConverBase {
+    def vldt                     (c: Cls): Errs    = vldtAsOrigin(c)
+    def _meta                    (c: Cls): Cls     = foldRpathz  (c) { _.updateInfo(_, Info.optBoolean) } // TODO: t210108114447 - support own "flag" type?
+    def atomuus(origin: CallSite)(c: Cls): AtomUUs = rpathz(c).pipe(_atoms(c)(_ConvertToFlag(origin)(_, trueValue, strict))) }
 
   // ---------------------------------------------------------------------------
-  case class ConvertToBoolean[T: WTT](target: TqRPathz, trueValue: T, falseValue: T) extends ActionUU1Nb {
-    def vldt                     (c: Cls): Errs    = target.vldtAsOrigin(c)
-    def _meta                    (c: Cls): Cls     = target.resolve(c).foldLeft(c) { _.transformSoleSubInfo(_)(_.toBoolean) }
-    def atomuus(origin: CallSite)(c: Cls): AtomUUs = target.resolve(c).pipe(_atoms(c)(_ConvertToBoolean(origin)(_, trueValue, falseValue))) }
+  case class ConvertToBoolean[T: WTT](target: TqRPathz, trueValue: T, falseValue: T) extends ActionUU1Nb with ConverBase {
+    def vldt                     (c: Cls): Errs    = vldtAsOrigin(c)
+    def _meta                    (c: Cls): Cls     = soleSubInfo (c)(_.toBoolean)
+    def atomuus(origin: CallSite)(c: Cls): AtomUUs = rpathz(c).pipe(_atoms(c)(_ConvertToBoolean(origin)(_, trueValue, falseValue))) }
 
   // ---------------------------------------------------------------------------
-  case class ConvertToOptionalBoolean[T: WTT](target: TqRPathz, trueValue: T, falseValue: T, nullValue: T) extends ActionUU1Nb {
-    def vldt                     (c: Cls): Errs    = target.vldtAsOrigin(c)
-    def _meta                    (c: Cls): Cls     = target.resolve(c).foldLeft(c) { _.updateInfo(_, Info.optBoolean) }
-    def atomuus(origin: CallSite)(c: Cls): AtomUUs = target.resolve(c).pipe(_atoms(c)(_ConvertToOptionalBoolean(origin)(_, trueValue, falseValue, nullValue))) }
+  case class ConvertToOptionalBoolean[T: WTT](target: TqRPathz, trueValue: T, falseValue: T, nullValue: T) extends ActionUU1Nb with ConverBase {
+    def vldt                     (c: Cls): Errs    = vldtAsOrigin(c)
+    def _meta                    (c: Cls): Cls     = foldRpathz  (c) { _.updateInfo(_, Info.optBoolean) }
+    def atomuus(origin: CallSite)(c: Cls): AtomUUs = rpathz(c).pipe(_atoms(c)(_ConvertToOptionalBoolean(origin)(_, trueValue, falseValue, nullValue))) }
 
   // ===========================================================================
   case class ToOptional(targets: TqRPathz, strict: Boolean) extends ActionUU0N with IdentityUU0N {
